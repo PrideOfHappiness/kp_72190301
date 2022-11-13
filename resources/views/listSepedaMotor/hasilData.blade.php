ded10974d373e55f4c843788f934b471e036cf99
Renaldi <soeryadi.renaldi@gmail.com>
2022-11-13 13:28:40 +0700

    Tambah Grafik

--- resources/views/listSepedaMotor/hasilData.blade.php
+++ resources/views/listSepedaMotor/hasilData.blade.php
@@ -4,42 +4,81 @@
 @include('template/sidebar')
 
 <section class="content">
-    <div class="container-fluid">
-        <div class="col-12">
-            <div class="card card-primary">
-                <div class="card-header">
-                        <h3 class="card-title"> Hasil Pencarian Data Dari Tanggal {{ $awal }} sampai Tanggal {{ $akhir }} </h3>
+   <div class="container-fluid" >
+        <div class="card card-default">
+            <div class="card-header">
+                <h3 class="card-title"> Hasil Pencarian Data Dari Tanggal {{ $awal }} sampai Tanggal {{ $akhir }} </h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
                 </div>
-                <div class="card-body">
-                    <table class="table table-bordered table-hover">
-                        <thead> 
-                            <tr> 
-                                <th> Nama Barang </th>
-                                <th> Jumlah Barang </th> 
-                            </tr>
-                        </thead>
-                        <tbody> 
+            </div>
+            <div class="card-body">
+                <table class="table table-borderless table-hover">
+                    <thead>
+                       <tr>
+                            <th> Nama Barang</th>
+                            <th> Jumlah </th>
+                       </tr>
+                    </thead>
+                    <tbody>
                         @if(!empty($proses) && $proses->count())
                             @foreach ($proses as $data=>$key)
-                            <tr> 
-                                <td> {{ ucfirst($data) }} </label></td>
-                                <td> {{ count($key) }}</td>
+                                <tr>
+                                    <td> {{ ucfirst($data) }} </label></td>
+                                    <td> {{ count($key) }}</td>
                                 @error('proses')
                                     <div class="invalid-feedback">
                                         {{ $message }}
                                     </div>
-                                    @enderror
-                            </tr>
+                                @enderror
+                                </tr>
                             @endforeach
                         @endif
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="card card-default">
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="far fa-chart-pie"> </i>
+                        Grafik Persentase
+                    </h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
                     </div>
+                </div>
+                <div class="card-body">
+                    <div id="donut-chart" style="height: 300px;"></div>
                 </div>
             </div>
         </div>
-    </div>
+   </div>
 </section>
-
 @include('template/footer')
 </body>
+
+<!-- Javascript untuk Grafik -->
+<script src="{{ asset('style/plugins/chart.js/Chart.min.js') }}"></script>
+
+<script>
+    var data = [
+        {
+            var donutChartCanvas = $('#donut-chart').get(0).getContext('2d')
+            labels = [
+
+            ]
+        }
+    ]
+</script>
